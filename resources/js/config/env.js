b1b5c8100a96a6fb16d411ec48bb266a4096e27c
mike <omodingmike@gmail.com>
2024-06-05 17:29:17 +0300

    removed files

--- resources/js/config/env.js
+++
@@ -1,8 +0,0 @@
-const ENV = {
-    // API_URL: process.env.MIX_HOST,
-    // API_URL:'https://smartduuka.test',
-    API_URL:'https://test.smartduuka.com',
-    GOOGLE_MAP_KEY: process.env.MIX_GOOGLE_MAP_KEY,
-    DEMO: process.env.MIX_DEMO
-};
-export default ENV;
